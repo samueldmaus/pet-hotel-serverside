e4db0f28ad1d7afcd95ad2bee4df2bba7e4cfc6d
Sam Maus <samueldmaus@gmail.com>
2020-09-23 16:07:36 -0500

    connected psycopg2 to db

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect("dbname = python-psycopg2 user=sammaus")
+cur = conn.cursor()
+
+@app.route('/hello')
+def hello():
+    cur.execute("SELECT * FROM test")
+    response = cur.fetchall()
+    print(response)
+    return jsonify({"result": response})
+    
+
+app.run(host='localhost', port=5000)
