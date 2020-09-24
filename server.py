d195beea983d421f1db3d6307433128676259f7c
Sam Maus <samueldmaus@gmail.com>
2020-09-24 10:46:13 -0500

    added psycopg2 extras to get an array of objects returned

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
 import psycopg2
+import psycopg2.extras
 
 app = Flask(__name__)
 
-conn = psycopg2.connect("dbname = python-psycopg2 user=sammaus")
+conn = psycopg2.connect("dbname = python-psycopg2 user=sammaus", cursor_factory=psycopg2.extras.RealDictCursor)
 cur = conn.cursor()
 
-@app.route('/hello', methods = ['GET', 'POST'] )
+@app.route('/hello', methods = ['GET', 'POST'])
 def hello():
     if request.method == 'GET':
         cur.execute("SELECT * FROM test")
@@ -16,10 +17,12 @@ def hello():
     else:
         name = request.form.get("name")
         data = request.form.get("data")
-        cur.execute(f"INSERT INTO test (name, data) VALUES ('{name}', {data});")
+        cur.execute("INSERT INTO test (name, data) VALUES (%s, %s);", {name, data})
         conn.commit()
-        return "success"
-    
+        return "success", 201
+
+
+
 
 app.run(host='localhost', port=5000)
 
